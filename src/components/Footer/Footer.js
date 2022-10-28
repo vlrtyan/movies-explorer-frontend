518bf61f1df2e583bad8f3ad52afe910a78be669
Valeria Tyan <vlrtyan@yandex.ru>
2022-10-28 13:55:37 +0900

    Revert "Merge branch 'main' into level-2"
    This reverts commit c21b8272f737d43ffd6c9dd94983ebd6b6c01dae, reversing
    changes made to 749a7bbaa71ff992d215ad13479b0b2586462a33.

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+    return (
+        <footer className='footer'>
+            <p className='footer__name'>Учебный проект Яндекс.Практикум х BeatFilm.</p>
+            <div className='copyright'>
+                <p className='copyright__year'>&copy; {new Date().getFullYear()}</p>
+                <ul className='copyright__links'>
+                    <li className='copyright__link'>
+                        <a className='copyright__link' href='https://practicum.yandex.ru/' target='_blank'>Яндекс.Практикум</a>
+                    </li>
+                    <li className='copyright__link'>
+                        <a className='copyright__link' href='https://github.com/' target='_blank'>Github</a>
+                    </li>
+                </ul>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
