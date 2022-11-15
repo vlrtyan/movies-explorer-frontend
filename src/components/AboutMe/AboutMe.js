ca81fbf3dbc539c4349dcbc8f894b24e7c6f9b24
Valeria Tyan <vlrtyan@yandex.ru>
2022-11-15 14:59:04 +0300

    bio

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,9 +1,9 @@
+/* eslint-disable react/jsx-no-target-blank */
 import React from 'react';
 import './AboutMe.css';
 import avatar from '../../images/avatar.jpg';
 
 function AboutMe() {
-    //написать био
     return (
         <section className='about-me'>
             <h2 className='about-me__header'>Студентка</h2>
@@ -11,8 +11,7 @@ function AboutMe() {
                 <div className='about-me__info'>
                     <p className='about-me__name'>Валерия</p>
                     <p className='about-me__occupation'>Фронтенд-разработчик, 23 года</p>
-                    <p className='about-me__bio'>Я родился и живу в Саратове, закончил факультет экономики СГУ. У меня есть жена
-                        и дочь. Я люблю слушать музыку, а ещё увлекаюсь бегом. Недавно начал кодить. С 2015 года работал в компании «СКБ Контур». После того, как прошёл курс по веб-разработке, начал заниматься фриланс-заказами и ушёл с постоянной работы.</p>
+                    <p className='about-me__bio'>Я живу в Санкт-Петербурге, закончила программу логистики НИУ ВШЭ. Я люблю футбол и фигурное катание. Недавно начала кодить, прошла курс Яндекс Практикума по веб-разаработке и сейчас начинаю карьеру фронтендера.</p>
                     <a className='about-me__github' href='https://github.com/vlrtyan' target='_blank'>Github</a>
                 </div>
                 <img className='about-me__photo' src={avatar} alt='Валерия Тян' />
